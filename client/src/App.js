c0eb5b956a7a7cabf726ddfed771ac767a2601d6
SMNAFI <sayem.md.nafi@gmail.com>
2022-10-10 19:03:59 +0600

    router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import './bootstrap.min.css'
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import LoginPage from './pages/LoginPage'
+import CartPage from './pages/CartPage'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/cart' element={<CartPage />} />
+          </Routes>
+          {/* <HomePage /> */}
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h3>Product Page</h3>
+    </div>
+  )
+}
+
+export default ProductPage
