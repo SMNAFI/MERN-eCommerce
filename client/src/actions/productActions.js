a81cb5ed2bfaacbbed261cab9f1b5fd63af8a364
SMNAFI <sayem.md.nafi@gmail.com>
2022-10-16 22:46:54 +0600

    product page with redux

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants'
 
 // listProducts is like action creator
@@ -16,6 +19,25 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        //   error.response.data.message this will give us error message of the custom error handling middleware
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    })
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const res = await axios.get(`http://localhost:5000/api/products/${id}`)
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: res.data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         //   error.response.data.message this will give us error message of the custom error handling middleware
         error.response && error.response.data.message
--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers'
 
 // as we have multiple reducer
 // productList acts a state
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 const initialState = {}
 // if we have multiple middleware
