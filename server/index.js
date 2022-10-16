4ce6709ae57e4c6cbd49bd7de2a48df50cad4310
SMNAFI <sayem.md.nafi@gmail.com>
2022-10-16 17:34:19 +0600

    routes and middleware for error handling added

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 // this is module type importing
-// so fileName.js must be included
+// so file extention must be included (.js)
 // in package.json
 // "type": "module"
 // needs to be added
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRountes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -25,14 +26,13 @@ app.get('/', (req, res) => {
   res.json('OHAIOO')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRountes)
 
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+// custom middleware if invalid path is accessed
+app.use(notFound)
+
+// overwriting the default error handler using custom error handler middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
